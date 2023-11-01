fe7bd75b31dac1856f232714c6c4b962f1d3100c
Tural Babirov <turalbabirov@mail.com>
2023-11-01 21:21:48 +0400

    reverse name task completed

---
+++ Lesson4/FirstWithC#/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+//using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace FirstWithC_
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine( reverseName("Suleyman", "Semed", "Tural", "Mirqubad") );
+        }
+
+        static string reverseName(params string[] adlar)
+        {
+            string total = "";
+
+            for (int i = 0; i < adlar.Length; i++)
+            {
+                for(int j=adlar[i].Length-1; j>=0; j--)
+                {
+                    total += adlar[i][j];
+                }
+                total += " ";
+            }
+
+            return total;
+        }
+    }
+}
