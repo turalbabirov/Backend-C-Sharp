0912ee6921bd3fcb30897e5e1610b939e00474fb
Tural Babirov <turalbabirov@mail.com>
2023-11-08 13:23:58 +0400

    Lesson 6 task finished

---
+++ HomeWorks/Lesson6/FirstWithC#/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Xml.Linq;
+
+namespace FirstWithC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            #region Task1 - Lesson 6:
+            //Student classınız olsun. int[] Grades propertysi olsun.
+            //Studentdən obyekt yaradıb grades-i set edəndə..
+            //..arrayin yalnız cüt ədədlərindən ibarət yeni bir array set olunsun.
+
+            //Yəni, əgər mən:
+            //Student st = new Student();
+            //st.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 } yazıramsa
+            //st.Grades { 2,4,6,8 }-ə bərabər olsun
+
+            Student obyekt = new Student();
+            obyekt.Grades = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            obyekt.GradeShow();
+
+
+            #endregion
+        }
+    }
+}
---
+++ HomeWorks/Lesson6/FirstWithC#/Student.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FirstWithC_
+{
+    internal class Student
+    {
+        private int[] _grades;
+        public int[] Grades { 
+            get {
+                return _grades;
+            } 
+            set {
+                int saygac = 0;
+
+                foreach (int element in value)
+                {
+                    if( element%2==0 )
+                    {
+                        saygac++;
+                    }
+                }
+
+                _grades = new int[saygac];
+
+                for(int i=0, j=0; i<value.Length; i++)
+                {
+                    if (value[i] % 2 == 0)
+                    {
+                        _grades[j++] = value[i];
+                    }
+
+                }
+
+                _grades = _grades;
+            } 
+        }
+        public void GradeShow() {
+            foreach (int elements in _grades)
+            {
+                Console.WriteLine(elements);
+            }
+        }
+    }
+}
