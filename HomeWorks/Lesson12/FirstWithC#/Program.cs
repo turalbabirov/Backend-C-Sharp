cfd49164247a8152e7bdea625ff3124fc69d88a8
Tural Babirov <turalbabirov@mail.com>
2023-11-21 19:28:28 +0400

    Lesson 12 task started

---
+++ HomeWorks/Lesson12/FirstWithC#/Program.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FirstWithC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //Custom Arraylist classı yazın,
+            //*Capacity, Count propları olsun,
+            //*Capacity qəbul edən constructoru olsun
+            //*Add metodu, Remove metodu, RemoveAt metodu, TrimToSize metodu, AddRange metodu olsun
+
+            //QEYD:
+            //Add Metodunu indiyə qədər neçə dəfə yazmışıq əslində.
+            //RemoveAt - da həmçinin sinifdə yazmışdıq.
+            //Digərlərini - də araşdırıb düşünün və yazın.
+            //Labdakı taskı tamamlamayanlar yazmağa çalışsın
+            //Olmasa bunu həll edin əsas.
+
+            //!! Listin öz metodlarından istifadə etməyin !!
+
+            Arraylist arrList = new Arraylist();
+            arrList.Add();
+
+        }
+    }
+
+    class Arraylist
+    {
+
+        public int Capacity;
+        public int Count;
+
+        public void Add()
+        {
+
+        }
+
+        public void Remove()
+        {
+
+        }
+
+        public void RemoveAt()
+        {
+
+        }
+
+        public void TrimToSize()
+        {
+
+        }
+
+        public void AddRange()
+        {
+
+        }
+    }
+}
