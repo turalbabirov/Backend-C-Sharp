ca127b010aaa43d6f0e6fa002cfd186ca108209c
Tural Babirov <turalbabirov@mail.com>
2023-11-29 21:04:50 +0400

    lesson16 not finished

---
+++ HomeWorks/Lesson16/FirstWithC#/Program.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FirstWithC_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            //PrintText adlı metodunuz olsun.
+            //Parametr olaraq string dəyər alsın və aldığı dəyəri console - a videodakı kimi çıxartsın
+
+            Thread thread1 = new Thread(PrintText);
+            thread1.Start("asadasd");
+
+        }
+
+        static void PrintText(string str)
+        {
+            Console.WriteLine(str);
+        }
+    }
+}
