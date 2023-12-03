c36d4aac055b4d664c257582ad297f8560cc0c78
Tural Babirov <turalbabirov@mail.com>
2023-12-03 17:30:46 +0400

    Asp.Net first lesson - Lesson 17 finished

---
+++ HomeWorks/Lesson17/FirstWithAspNet/Controllers/User.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWithAspNet.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Name1 = "Mark";
+            ViewBag.Surname1 = "Otto";
+            ViewBag.Age1 = 21;
+            ViewBag.Country1 = "England";
+            ViewBag.PhoneNumber1 = "1212631823981";
+
+            ViewBag.Name2 = "Jacob";
+            ViewBag.Surname2 = "Thornton";
+            ViewBag.Age2 = 31;
+            ViewBag.Country2 = "Island";
+            ViewBag.PhoneNumber2 = "213123123124";
+
+            ViewBag.Name3 = "Larry";
+            ViewBag.Surname3 = "The Bird";
+            ViewBag.Age3 = 41;
+            ViewBag.Country3 = "Scotland";
+            ViewBag.PhoneNumber3 = "331232424123";
+
+            return View();
+        }
+    }
+}
